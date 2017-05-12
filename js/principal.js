c174fff5da0ddb49e7aa78f1f3921817c2017200
Pedro Lazari <plazari96@gmail.com>
2017-05-11 22:31:29 -0300

    testes e validação

--- js/principal.js
+++ js/principal.js
@@ -1,9 +1,19 @@
+console.error('Exibindo erro');
+console.info('Exibindo info');
+console.sucess('Exibindo sucesso');
 document.querySelector('.titulo').textContent = 'Aparecida Nutricionista';
 
 var paciente = document.querySelector("#primeiro-paciente");
 
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
+
+//Calculando o peso
+if(peso <= 0)
+  console.log('Peso inválido');
+
+if(peso >= 800)
+  console.error('Peso Inválido')
 
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
