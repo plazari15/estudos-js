3fb1051f6e1e1cdfa021dcfbdedc8d501a22a8db
Pedro Lazari <plazari96@gmail.com>
2017-05-11 22:33:56 -0300

    Refatorando JS

--- js/principal.js
+++ js/principal.js
@@ -7,11 +7,8 @@ var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
 //Calculando o peso
-if(peso <= 0)
+if(peso <= 0 || peso >= 800)
   console.log('Peso inválido');
-
-if(peso >= 800)
-  console.log('Peso Inválido')
 
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
