85b6fdba0d17557c5ba46a6d15296f1e03f0bf66
Pedro <plazari96@gmail.com>
2017-05-12 14:40:55 -0300

    Aplicando um mini Loop

--- js/principal.js
+++ js/principal.js
@@ -1,31 +1,34 @@
 document.querySelector('.titulo').textContent = 'Aparecida Nutricionista';
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-var PesoValido = true;
-var AlturaValida = true;
+var pacientes = document.querySelectorAll(".paciente");
 
-//Calculando o peso
-if(peso <= 0 || peso >= 800){
-  console.log('Peso inválido');
-  var PesoValido = false;
+
+//Vai rodar o for
+for (var i = 0; i <= pacientes.length; i++) {
+	var paciente = pacientes[i];
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
+	var PesoValido = true;
+	var AlturaValida = true;
+
+	//Calculando o peso
+	if(peso <= 0 || peso >= 800){
+	  console.log('Peso inválido');
+	  var PesoValido = false;
+	}
+
+	if(altura <= 0 || altura >= 3.00) {
+	  console.log('Altura inválido');
+	  alturaEhValida = false;
+	}
+
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
+	var tdImc = paciente.querySelector(".info-imc");
+	var imc = peso / (altura * altura);
+	if(PesoValido && AlturaValida)
+	  tdImc.textContent = imc;
 }
 
-if(altura <= 0 || altura >= 3.00) {
-  console.log('Altura inválido');
-  alturaEhValida = false;
-}
-
-
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-
-var tdImc = paciente.querySelector(".info-imc");
-
-var imc = peso / (altura * altura);
-
-if(PesoValido && AlturaValida)
-  tdImc.textContent = imc;
 
 
 
